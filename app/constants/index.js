80c6a22781a9464f40b41be7b58d725f6b2413b2
Dmytro Zhuravlov <Dmytro_Zhuravlov@epam.com>
2016-10-03 20:59:31 +0300

    add constants

--- app/constants/index.js
+++ app/constants/index.js
@@ -2,3 +2,6 @@ export const WEATHER_LIST = 'Weather List';
 export const AUTOCOMPLETE_LIST = 'Autocomplete List';
 
 export const STORE_NAME = 'weatherItems';
+
+export const GEO_LOCATION_SERVICE_URL = 'https://maps.google.com/maps/api/geocode/json?sensor=false&address=';
+export const WEATHER_SERVICE_URL = 'https://api.forecast.io/forecast';
--- app/services/index.js
+++ app/services/index.js
@@ -1,10 +1,11 @@
 import fetchJsonp from 'fetch-jsonp';
 import axios from 'axios';
+import {GEO_LOCATION_SERVICE_URL, WEATHER_SERVICE_URL} from '../constants/index.js';
 
 export const GetWeather = (lat, lng, name, callback) => {
     const API_KEY = 'e6b2ec46c1a1424d28fd7606c38272c6';
 
-    return fetchJsonp(`https://api.forecast.io/forecast/${API_KEY}/${lat},${lng}`)
+    return fetchJsonp(`${WEATHER_SERVICE_URL}/${API_KEY}/${lat},${lng}`)
     .then((response) => {
         return response.json()
     })
@@ -16,6 +17,6 @@ export const GetWeather = (lat, lng, name, callback) => {
 
 export const GetLocation = (targetVal, callback) => {
     axios
-    .get('https://maps.google.com/maps/api/geocode/json?sensor=false&address=' + targetVal)
+    .get(GEO_LOCATION_SERVICE_URL + targetVal)
     .then(data => callback(data));    
 }
